6ecb6cf62edd1f03092752aefd95189c26a4ff32
Jack Kinga <jackmwangi@gmail.com>
2016-05-07 09:55:06 +0300

    can create project from file contents

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"github.com/kingzbauer/taskmanager/utils"
+	"flag"
+	"fmt"
 	"io/ioutil"
 )
 
+var taskfile string
+
+func init() {
+	flag.StringVar(&taskfile, "taskfile", "tasks.yml", "path to your tasks file")
+}
+
 func main() {
+	flag.Parse()
 	// read the tasks.yml file contents
-	contents, err := ioutil.ReadFile("tasks.yml")
+	contents, err := ioutil.ReadFile(taskfile)
+	handleError(err, true)
+
+	// create a project
+	project := NewProjectFromFile(contents)
+	fmt.Println(project)
 }
--- models.go
+++ models.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"gopkg.in/yaml.v2"
+)
+
 // Task represents a single runnable command
 type Task struct {
 	Name    string   `yaml:"name"`
@@ -13,4 +17,13 @@ type Project struct {
 	Name       string `yaml:"project"`
 	WorkingDir string `yaml:"working_dir"`
 	Tasks      []Task `yaml:"tasks"`
+}
+
+// NewProjectFromFile creates a new project from the contents of a file
+func NewProjectFromFile(fileContents []byte) *Project {
+	project := new(Project)
+	err := yaml.Unmarshal(fileContents, project)
+	handleError(err, true)
+
+	return project
 }
